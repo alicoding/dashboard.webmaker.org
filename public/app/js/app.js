612def397eb78e07e175cb533b443f0a01fe7e9f
ksedge <kieran.sedgwick@gmail.com>
2013-10-18 12:27:32 -0400

    First swing at angularjs splattr app, and added bootstrap.css

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -2,15 +2,18 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
+angular.module('splattrApp', [
   'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+  'splattrApp.filters',
+  'splattrApp.services',
+  'splattrApp.directives',
+  'splattrApp.controllers'
+]);
+
+// TODO: Split into views
+//
+// .config(['$routeProvider', function($routeProvider) {
+//   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+//   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+//   $routeProvider.otherwise({redirectTo: '/view1'});
+// }]);
