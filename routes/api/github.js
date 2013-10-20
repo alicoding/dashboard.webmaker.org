d1fcec4d0e20c9114baf98588c1db112139e30e9
Rick Eyre <rick.eyre@hotmail.com>
2013-10-20 11:44:02 -0400

    Rename variables to avoid naming collision.

--- routes/api/github.js
+++ routes/api/github.js
@@ -4,74 +4,74 @@ module.exports = function( cache ) {
 
   return {
     tags: function( req, res ) {
-      GitHub.tags( req.params.repo, function( err, tags ) {
+      GitHub.tags( req.params.repo, function( err, data ) {
         if ( err ) {
           res.json( 500, { error: 'Unable to get tags for repo ' + req.params.repo + '.' } );
           return;
         }
-        cache.write( req.url, tags );
-        res.json( tags );
+        cache.write( req.url, data );
+        res.json( data );
       });
     },
     tagsFromDate: function( req, res ) {
-      GitHub.tagsFromDate( req.params.repo, req.params.date, function( err, tags ) {
+      GitHub.tagsFromDate( req.params.repo, req.params.date, function( err, data ) {
         if ( err ) {
           res.json( 500, { error: 'Unable to get tags for repo ' + req.params.repo +
                            ' from date ' + req.params.date + '.' } );
           return;
         }
-        cache.write( req.url, tags );
-        res.json( tags );
+        cache.write( req.url, data );
+        res.json( data );
       });
     },
     commits: function( req, res ) {
-      GitHub.commits( req.params.repo, function( err, commits ) {
+      GitHub.commits( req.params.repo, function( err, data ) {
         if ( err ) {
           res.json( 500, { error: 'Unable to get commits for repo ' + req.params.repo + '.' } );
           return;
         }
-        cache.write( req.url, commits );
-        res.json( commits );
+        cache.write( req.url, data );
+        res.json( data );
       });
     },
     commitCounts: function( req, res ) {
-      GitHub.commitCounts( function( err, counts ) {
+      GitHub.commitCounts( function( err, data ) {
         if ( err ) {
           res.json( 500, { error: 'Unable to component commit counts.' } );
           return;
         }
-        cache.write( req.url, counts );
-        res.json( counts );
+        cache.write( req.url, data );
+        res.json( data );
       });
     },
     contributors: function( req, res ) {
-      GitHub.contributors( req.params.repo, function( err, contributors ) {
+      GitHub.contributors( req.params.repo, function( err, data ) {
         if ( err ) {
           res.json( 500, { error: 'Unable to get contributors for repo ' + req.params.repo + '.' } );
           return;
         }
-        cache.write( req.url, contributors );
-        res.json( contributors );
+        cache.write( req.url, data );
+        res.json( data );
       });
     },
     contributorCounts: function( req, res ) {
-      GitHub.contributorCounts( function( err, counts ) {
+      GitHub.contributorCounts( function( err, data ) {
         if ( err ) {
           res.json( 500, { error: 'Unable to get component contributor counts.' } );
           return;
         }
-        cache.write( req.url, counts );
-        res.json( counts );
+        cache.write( req.url, data );
+        res.json( data );
       });
     },
     summaries: function( req, res ) {
-      GitHub.summaries( function( err, summaries ) {
+      GitHub.summaries( function( err, data ) {
         if ( err ) {
           res.json( 500, { error: 'Unable to get component summaries.' } );
           return;
         }
-        cache.write( req.url, summaries );
-        res.json( summaries );
+        cache.write( req.url, data );
+        res.json( data );
       });
     }
   };
