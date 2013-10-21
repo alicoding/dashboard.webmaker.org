71c3dfb7c8a1e6edfcb95426b5a717e02122eb06
ksedge <kieran.sedgwick@gmail.com>
2013-10-21 16:34:31 -0400

    Added single-page deep routing plus an example of using route parameters for advanced calls

--- public/app/js/controllers.js
+++ public/app/js/controllers.js
@@ -1,18 +1,39 @@
-'use strict';
+"use strict";
 
 /* Controllers */
 
-var controllers = angular.module('splattrApp.controllers', [])
-  .controller('BugSplattrController', [ '$scope', "Bugs", function( $scope, Bugs ) {
+var controllers = angular.module("splattrApp.controllers", []);
+
+  controllers.controller("RootSplattrController", [ "$scope", "Bugs", "Github", "Transifex", function( $scope, Bugs, Github, Transifex ) {
     // Use custom service to query data from the server (see services.js)
-    $scope.bugs = Bugs.componentCounts.query();
+
+    // This controller will collect metadata about transifex & bugs overall (all components)
   }])
-  .controller('GitSplattrController',  [ '$scope', "Github", function( $scope, Github ) {
+  .controller("BugSplattrController", [ "$scope", "Bugs", "Github", function( $scope, Bugs, Github ) {
     // Use custom service to query data from the server (see services.js)
+    // NOTE: $routeParams isn't used in this controller.  If you need access to it,
+    //       add it to the declaration like in `BugCompSplatterController`
+    $scope.bugs = Bugs.componentCounts.query();
     $scope.github = Github.componentSummaries.query();
   }])
-  .controller('TransifexSplattrController',  [ '$scope', "Transifex", function( $scope, Transifex ) {
+  .controller("TransifexSplattrController",  [ "$scope", "Transifex", function( $scope, Transifex ) {
     // Use custom service to query data from the server (see services.js)
-    $scope.contributions = Transifex.listOfContributors.query();
-    $scope.languages = Transifex.languages.query();
+    $scope.transifex = $scope.transifex || {};
+
+    // NOTE: $routeParams isn't used in this controller.  If you need access to it,
+    //       add it to the declaration like in `BugCompSplatterController`
+    $scope.transifex.contributions = Transifex.listOfContributors.query();
+    $scope.transifex.languages = Transifex.languages.query();
+  }])
+  .controller("TransifexCompSplattrController",  [ "$scope", "$routeParams", "Transifex", function( $scope, $routeParams, Transifex ) {
+    // Use custom service to query data from the server (see services.js)
+
+    // This controller will collect transifex data about a specific component.
+    // See "BugCompSlattrController", which uses `services.js` and `controllers.js` in tandem
+    // to get the parameter from the route defined in `app.js`
+  }])
+  .controller("BugCompSplattrController", [ "$scope", "$routeParams", "Bugs", "Github", function( $scope, $routeParams, Bugs, Github ) {
+    // Use custom service to query data from the server (see services.js)
+    $scope.component = $routeParams.component;
+    $scope.bugs = Bugs.openBugsByComponent.get({ component: $routeParams.component });
   }]);
