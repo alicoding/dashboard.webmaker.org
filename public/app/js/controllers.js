d440f8fa073f6c52100798219b8ff9221fd86e00
Rick Eyre <rick.eyre@hotmail.com>
2013-10-20 21:00:52 -0400

    Service factories should be able to return more then one service.

--- public/app/js/controllers.js
+++ public/app/js/controllers.js
@@ -5,13 +5,13 @@
 var controllers = angular.module('splattrApp.controllers', [])
   .controller('BugSplattrController', [ '$scope', "Bugs", function( $scope, Bugs ) {
     // Use custom service to query data from the server (see services.js)
-    $scope.bugs = Bugs.query();
+    $scope.bugs = Bugs.componentCounts.query();
   }])
   .controller('GitSplattrController',  [ '$scope', "Github", function( $scope, Github ) {
     // Use custom service to query data from the server (see services.js)
-    $scope.github = Github.query();
+    $scope.github = Github.componentSummaries.query();
   }])
   .controller('TransifexSplattrController',  [ '$scope', "Transifex", function( $scope, Transifex ) {
     // Use custom service to query data from the server (see services.js)
-    $scope.transifex = Transifex.query();
+    $scope.transifex = Transifex.listOfContributors.query();
   }]);
--- public/app/js/services.js
+++ public/app/js/services.js
@@ -6,36 +6,38 @@ var splattrAppServices = angular.module('splattrApp.services', [ 'ngResource' ])
   .value('version', '0.1');
 
 // These can declared as dependancies to be injected in any controller
-splattrAppServices.factory('Bugs', ['$resource',
-  function($resource){
-    return $resource('/bugzilla/components/counts', {}, {
+splattrAppServices.factory('Bugs', ['$resource', function($resource){
+  return {
+    componentCounts: $resource('/bugzilla/components/counts', {}, {
       query: {
-        method:'GET',
+        method: 'GET',
         params: {},
         isArray: true
       }
-    });
-  }]);
+    })
+  };
+}]);
 
-splattrAppServices.factory('Github', ['$resource',
-  function($resource){
-    return $resource('/github/components/summaries', {}, {
+splattrAppServices.factory('Github', ['$resource', function($resource){
+  return {
+    componentSummaries: $resource('/github/components/summaries', {}, {
       query: {
-        method:'GET',
+        method: 'GET',
         params: {},
         isArray: true
       }
-    });
-  }]);
+    })
+  };
+}]);
 
-splattrAppServices.factory('Transifex', ['$resource',
-  function($resource){
-    return $resource('/transifex/listOfContributors', {}, {
+splattrAppServices.factory('Transifex', ['$resource', function($resource){
+  return {
+    listOfContributors: $resource('/transifex/listOfContributors', {}, {
       query: {
-        method:'GET',
+        method: 'GET',
         params: {},
         isArray: true
       }
-    });
-  }]);
-
+    })
+  };
+}]);
